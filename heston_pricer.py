c30ffafa0553cd6acda05c1c846b1ba17eac8282
Krupam Patel <x16pat@gmail.com>
2025-12-25 10:53:33 -0600

    Refactor Pricer class to pricer and update methods

--- heston_pricer.py
+++ heston_pricer.py
@@ -1,10 +1,12 @@
 """Heston Option Pricing Engine: exotic and vanilla"""
+"""
+DONE
+"""
 import numpy as np
 
-class Pricer:
+class pricer:
     def __init__(self, model):
         self.model = model
-
 
     def european(self, T, S0, r, q, K, type='Call'):
         call_price = self.model.heston_call(T, S0, r, q, K)
@@ -13,31 +15,30 @@ class Pricer:
 
 
     def digital(self, T, S0, r, q, K, type='Call', npaths=250000, seed=None):
-        S_t, _ = self.model.simulate(S0, T, r, q, npaths=npaths, nsteps=252, seed=seed)
-        S_T = S_t[-1, :]
+        S_paths, _ = self.model.simulate(S0, T, r, q, npaths=npaths, nsteps=252, seed=seed)
+        S_first = S_paths[-1, :]
 
         if type == 'Call':
-            price = np.exp(-r*T) * np.mean(S_T > K)
+            price = np.exp(-r*T) * np.mean(S_first > K)
         else:
-            price = np.exp(-r*T) * np.mean(S_T < K)
+            price = np.exp(-r*T) * np.mean(S_first < K)
         return price
-
 
     def barrier(self, T, S0, r, q, K, B, barrier_type='UpAndOut', option_type='Call',
                 npaths=250000, nsteps=365, seed=None):
-        S_t, _ = self.model.simulate(S0, T, r, q, npaths=npaths, nsteps=nsteps, seed=seed)
+        S_paths, _ = self.model.simulate(S0, T, r, q, npaths=npaths, seed=seed)
 
         if barrier_type in ['UpAndOut', 'UpAndIn']:
-            barrier_hit = np.any(S_t >= B, axis=0)
+            barrier_hit = S_paths.max(axis=0) >= B
         elif barrier_type in ['DownAndOut', 'DownAndIn']:
-            barrier_hit = np.any(S_t <= B, axis=0)
+            barrier_hit = S_paths.min(axis=0) <= B
         else:
             raise ValueError("Invalid barrier type.")
 
         if option_type == 'Call':
-            payoff = np.maximum(S_t[-1, :] - K, 0)
+            payoff = np.maximum(S_paths[-1, :] - K, 0)
         elif option_type == 'Put':
-            payoff = np.maximum(K - S_t[-1, :], 0)
+            payoff = np.maximum(K - S_paths[-1, :], 0)
         else:
             raise ValueError("Invalid option type.")
 
@@ -49,5 +50,5 @@ class Pricer:
         price = np.exp(-r*T) * np.mean(payoff)
         return price
     
-
-print("HestonPricer works")
+if __name__ == "__main__":
+    print("HestonPricer works")
